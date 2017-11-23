b3c0df87d9b538eb75a2f5c6942a54a4a1f4efad
Uzi Landsmann <uzi.landsmann@gmail.com>
2017-11-23 22:23:09 +0100

    Added function exercises and tests

--- src/main/kotlin/se/webstep/kotlinworkshop/part_2_functions/exercise.kt
+++ src/main/kotlin/se/webstep/kotlinworkshop/part_2_functions/exercise.kt
@@ -1,18 +1,33 @@
 package se.webstep.kotlinworkshop.part_2_functions
 
-val strangeSentence = "I have never thought I'm gonna need to give blood again. You saw me up there, right?"
+/**
+ * Create a function concat that concatenates two strings
+ */
+fun concat(s1: String, s2: String) = s1 + s2
 
-fun split(s: String) = s.split(" ").toList()
-fun getEveryThirdWord(arr: List<String>) = arr
-        .zip(1..arr.size)
-        .filter { (it.second) % 3 == 0 }
-        .map { it.first }
-        .joinToString(" ")
-        .toLowerCase()
-
-fun main(args: Array<String>) {
-    val sp = split(strangeSentence)
-    println("sp: " + sp)
-    val res = getEveryThirdWord(sp)
-    println("res: " + res)
+/**
+ * Create a function concatDefaults that concatenates two strings defaulting to "Point" and "Less"
+ */
+fun concatDefaults(s1: String = "Point", s2: String = "less"): String {
+    return concat(s1, s2)
 }
+
+/**
+ * Create a function mangleNumbers with the following arguments:
+ * - two numbers
+ * - a function that takes two numbers and returns a number. It should be applied to the first two arguments and return the result
+ * - a function that takes one number and returns a number. It should be applied to the result of the first function and return the result
+ * The function should return the result of the second function
+ */
+fun mangleNumbers(x: Int, y: Int, first: (Int, Int) -> Int, then: (Int) -> Int): Int {
+    val doThis = first(x, y)
+    val thenThis = then(doThis)
+    return thenThis
+}
+
+/**
+ * Creates a function concatenator that:
+ * - takes a string as argument
+ * - returns a function that takes another string and returns both string concatenated
+ */
+fun concatenator(s1: String): (String) -> String = { s2 -> concat(s1, s2)}
---
+++ src/test/kotlin/part_2_functions_tests.kt
@@ -0,0 +1,48 @@
+import io.kotlintest.matchers.shouldBe
+import io.kotlintest.specs.StringSpec
+import se.webstep.kotlinworkshop.part_2_functions.concat
+import se.webstep.kotlinworkshop.part_2_functions.concatDefaults
+import se.webstep.kotlinworkshop.part_2_functions.concatenator
+import se.webstep.kotlinworkshop.part_2_functions.mangleNumbers
+
+class ConcatenateTests : StringSpec({
+    "Hello and World should be Hello World" {
+        concat("Hello ", "World") shouldBe "Hello World"
+    }
+})
+
+class ConcatenateDefaultTests : StringSpec({
+    "Using defaults should be Pointless" {
+        concatDefaults() shouldBe "Pointless"
+    }
+
+    "Heart and default should be Heartless" {
+        concatDefaults(s1 = "Heart") shouldBe "Heartless"
+    }
+
+    "default and blank should be Point blank" {
+        concatDefaults(s2 = " blank") shouldBe "Point blank"
+    }
+})
+
+class MangleNumbersTests : StringSpec({
+    "(2 + 4) * 5 should be 30" {
+        mangleNumbers(2, 4, Int::plus, { it * 5 }) shouldBe 30
+    }
+
+    "3 * 4 + 10 should be 22" {
+        mangleNumbers(3, 4, Int::times, { it + 10 }) shouldBe 22
+    }
+
+    "81 / 9 / 3 should be 3" {
+        mangleNumbers(81, 9, Int::div, { it / 3 }) shouldBe 3
+    }
+})
+
+class PartialConcatTests : StringSpec({
+    "partial function fooConcatenator should concatenate correctly" {
+        val fooConcatenator = concatenator("Foo")
+        fooConcatenator("bar") shouldBe "Foobar"
+        fooConcatenator(" Fighters") shouldBe "Foo Fighters"
+    }
+})
