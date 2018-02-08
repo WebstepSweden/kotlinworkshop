f79c12c5128cd2d93153404f41e9b3f741107841
Uzi Landsmann <uzi.landsmann@gmail.com>
2018-02-08 15:22:47 +0100

    changed build to gradle kotlin dsl
    (cherry picked from commit 17aca90)

---
+++ build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    kotlin("jvm") version "1.2.21"
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    testCompile("io.kotlintest:kotlintest:2.0.7")
+}
+
+repositories {
+    jcenter()
+}
