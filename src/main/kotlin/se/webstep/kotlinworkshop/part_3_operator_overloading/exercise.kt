d0f5d1086fcf4418965bb0da44e47e9b9b18b0a5
Uzi Landsmann <uzi.landsmann@gmail.com>
2018-06-17 17:36:24 +0200

    replaced if with require

--- src/main/kotlin/se/webstep/kotlinworkshop/part_3_operator_overloading/exercise.kt
+++ src/main/kotlin/se/webstep/kotlinworkshop/part_3_operator_overloading/exercise.kt
@@ -2,9 +2,7 @@ package se.webstep.kotlinworkshop.part_3_operator_overloading
 
 class Fraction(val numerator: Int, val denominator: Int) {
     init {
-        if (denominator == 0) {
-            throw IllegalArgumentException("Division by zero")
-        }
+        require(denominator != 0) { "Division by zero" }
     }
 
     override fun toString(): String = "$numerator/$denominator"
